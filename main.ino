d16ab092f9c8495fffeb213b133ef35adb77851b
RomainCuchet <47221720+RomainCuchet@users.noreply.github.com>
2021-05-07 22:09:55 +0200

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,12 +1,9 @@
 const byte ultrasound_sensor_trigger_pin = 2;
 const byte ultrasound_sensor_echo_pin = 4;
-const byte infrared_sensor_right_pin = ;
-const byte infrared_sensor_left_pin = ;
+const byte buzzer_pin = 7;
 long lecture_echo; // long[2^31-1;2^31-1]
 long obstacle_distance;
 bool is_obstacle = false; // true if there is an obstacle
-bool infrared_sensor_left_state; // HIGH or LOW
-bool infrared_sensor_right_state; // HIGH or LOW
 
 long distance_mesurement(){
   digitalWrite(ultrasound_sensor_trigger_pin, HIGH);
@@ -15,35 +12,6 @@ long distance_mesurement(){
   lecture_echo = pulseIn(ultrasound_sensor_echo_pin,HIGH);
   obstacle_distance = lecture_echo /58;
   return obstacle_distance;
-}
-
-void line_follower(){
-  infrared_sensor_right_state = digitalRead(infrared_sensor_right_pin)
-  infrared_sensor_left_state = digitalRead(infrared_sensor_left_pin)
-  if (infrared_sensor_right_state == LOW and infrared_sensor_left_state == LOW){
-    forward()
-  }
-  else if (infrared_sensor_right_state == HIGH and infrared_sensor_left_state == LOW){
-    turn_right(degree = 5)
-  }
-  else if (infrared_sensor_right_state == LOW and infrared_sensor_left_state == HIGH){
-    turn_left(degree = 5)
-  }
-  else if (infrared_sensor_right_state == HIGH and infrared_sensor_left_state == HIGH){
-    turn_(degree = 90)// à déterminer
-  }
-}
-void turn_left(degree){
-
-}
-void turn_right(degree){
-  
-}
-void forward(){
-  
-}
-void backward(){
-  
 }
 
 void Serialprint(long obstacle_distance){
@@ -56,14 +24,13 @@ void setup(){
   pinMode(ultrasound_sensor_trigger_pin, OUTPUT);
   digitalWrite(ultrasound_sensor_trigger_pin, LOW);
   pinMode(ultrasound_sensor_echo_pin, INPUT);
-  pinMode(infrared_sensor_left_pin, INPUT)
-  pinMode(infrared_sensor_right_pin, INPUT)
+  pinMode(buzzer_pin, OUTPUT);
   Serial.begin(9600);
-
 }
 
 void loop(){
   obstacle_distance = distance_mesurement();
   Serialprint(obstacle_distance);
-  delay(1500);
+  update_buzzer(obstacle_distance);
+  delay(1000);
 }
